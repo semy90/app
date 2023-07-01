556c14c162ff8ac7ed37bee0a310d0cb1d98eb8c
Semyon Glinskikh <semenglinskikh@gmail.com>
2023-07-01 15:09:06 +0500

    am add new funs(multy) and del var(text) also i created new file(str.py)

--- le/noose.py
+++ le/noose.py
@@ -2,13 +2,15 @@ def plusse(a, b):
     return a + b
 
 
-text = 'new text'
-
-
 def minus(a, b):
     return a - b
+
+
+def multy(a, b):
+    return a * b
 
 
 if __name__ == "__main__":
     print(plusse(2, 2))
     print(minus(2, 2))
+    print(multy(2, 2))
